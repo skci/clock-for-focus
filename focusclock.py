6faa912067c69f52170a3b081154fb0fff8ea035
@xiaokkk <31680619+skci@users.noreply.github.com>
2023-12-10 22:23:52 +0800

    更新 focusclock.py

--- focusclock.py
+++ focusclock.py
@@ -13,4 +13,6 @@ def focus_timer(minutes):
         print(f"Remaining time: {seconds // 60} minutes {seconds % 60} seconds")
         time.sleep(1)
         seconds -= 1
+        
+        
     print("Time's up! Good job focusing.")
